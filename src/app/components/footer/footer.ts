2a22b00c6e49b31b7cefed239dfe0c8ac557c2be
Kaylee <s1152274@student.hsleiden.nl>
2026-03-22 17:11:35 +0100

    Chore: add footer copyright

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   imports: [
     RouterLink,
+    DatePipe,
   ],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
-
+export class Footer {
+  protected today: Date = new Date();
+}
