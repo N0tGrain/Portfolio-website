ba26bf315454f58dd13576ca1cd9a90805389238
Kaylee <s1152274@student.hsleiden.nl>
2026-03-22 15:08:36 +0100

    Feat: add basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {Home} from './pages/home/home';
+import {About} from './pages/about/about';
+import {Projects} from './pages/projects/projects';
+import {Contact} from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+  // Possibly add a project information page?
+  {
+    path: 'contact',
+    component: Contact,
+  },
+  {
+    path: '**',
+    component: Home,
+  },
+];
